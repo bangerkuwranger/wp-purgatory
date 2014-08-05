cc286d1c855d8d658db466ac09c3d186a01586d7
Chad A. Carino <chadc@tekserve.com>
2014-08-04 23:42:14 -0400

    Early templete and variable updates

--- lib/extras.php
+++ lib/extras.php
@@ -20,3 +20,12 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
+
+/**
+ * VT323 - Google Font
+ */
+function load_vt323() {
+    wp_register_style('vt323', 'http://fonts.googleapis.com/css?family=VT323'_;
+    wp_enqueue_style( 'vt323');
+  }
+add_action('wp_print_styles', 'load_vt323');
